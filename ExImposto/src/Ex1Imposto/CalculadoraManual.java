0d12c5428447020d19a9f95ccbc7c2fb74582702
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-02-21 22:22:35 -0300

    criando métodos no Inss

--- ExImposto/src/Ex1Imposto/CalculadoraManual.java
+++ ExImposto/src/Ex1Imposto/CalculadoraManual.java
@@ -8,11 +8,14 @@ public class CalculadoraManual {
 		double salarioLiquido = 0.0;
 		
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Digite seu salário");
+		System.out.println("Digite seu salario");
 		double salarioBruto = Double.parseDouble(keyboard.nextLine());
 		
 		// Implemente aqui
-		
+		Inss inss = new Inss(salarioBruto);
+		double descontoInss = inss.calcularDesconto();
+		System.out.println("Desconto INSS: " + descontoInss);
+
 		System.out.println("Salario Liquido: " + salarioLiquido);
 	}
 
---
+++ ExImposto/src/Ex1Imposto/Inss.java
@@ -0,0 +1,13 @@
+package Ex1Imposto;
+
+public class Inss {
+	private double salarioBruto;
+	
+	public Inss(double salarioBruto) {
+		this.salarioBruto = salarioBruto;
+	}
+	
+	public double calcularDesconto() {
+		return 0.0;
+	}
+}
